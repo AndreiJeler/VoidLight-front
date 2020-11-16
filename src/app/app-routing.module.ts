538271bf1257233d8e1d5856fc812c754c3a93ad
Stefan Guliciuc <stefanguliciuc0@gmail.com>
2020-11-16 20:43:14 +0200

    T16 post front end (#21)
    * post component - wip
    
    * <11-11-2020><First Version of Post>
    
    * fixed buttons
    
    * <2020-11-16><Removed some lines and checked it>
    
    Co-authored-by: denisa.luca <denisa_luca99@yahoo.com>
    Co-authored-by: Stefan Guliciuc <stefan.guliciuc@fabritglobal.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostComponent } from './components/post/post.component';
+import { LoginComponent } from '../app/components/login/login.component'
 
-import {LoginComponent} from '../app/components/login/login.component'
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent },
+  { path: 'post', component: PostComponent }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './components/post/post.component';
 
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
@@ -13,13 +14,14 @@ import { LoginComponent } from './components/login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    PostComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
