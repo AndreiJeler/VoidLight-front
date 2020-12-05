0165bc4ec1b44a3961afdb8a6414fea0bdde7bfb
qGeo <loba.georgiana@yahoo.ro>
2020-12-05 22:24:56 +0200

    Fix bootstrap

--- src/app/components/newsfeed/newsfeed.component.ts
+++ src/app/components/newsfeed/newsfeed.component.ts
@@ -1,6 +1,7 @@
 import { SwalService } from './../../shared/services/swal.service';
 import { Constants } from './../../shared/utils/constants';
 import {
+  AfterViewInit,
   ChangeDetectorRef,
   Component,
   OnInit,
@@ -15,12 +16,14 @@ import { PostService } from '../../services/post.service';
 import { GameService } from '../../services/game.service';
 import { PublisherService } from '../../services/publisher.service';
 import { FriendsService } from '../../services/friends.service';
+import { DomService } from '../../services/dom.service';
 
 import { Post } from 'src/app/models/post';
 import { User } from 'src/app/models/user';
 import { Game } from 'src/app/models/game';
 import { Publisher } from 'src/app/models/publisher';
 import Swal from 'sweetalert2';
+import { CreatePostComponent } from './modals/create-post/create-post.component';
 
 @Component({
   selector: 'app-newsfeed',
@@ -42,9 +45,11 @@ export class NewsfeedComponent implements OnInit {
     private _friendsService: FriendsService,
     private _authenticationService: AuthenticationService,
     private _modalService: BsModalService,
+    private _domService: DomService,
     private _router: Router,
     private cdr: ChangeDetectorRef
   ) {}
+
 
   ngOnInit(): void {
     if (!this._authenticationService.currentUserValue) {
@@ -174,152 +179,19 @@ export class NewsfeedComponent implements OnInit {
   //start: Modal Operations Region
 
   public async openModalCreatePost(selectedModal: TemplateRef<any>) {
-    //this.chosenModal = this._modalService.show(selectedModal);
-    const { value: formValues } = await Swal.fire({
-      title: 'Add a text post',
-      html:
-        '<textarea id="swal-input1" class="swal2-input"></textarea>' +
-        '<select id="swal-input2" class="swal2-input"><option value="No game">No game</option>' +
-        '<option value="Gta V">Gta V</option><option value="Gta San Andreas">Gta San Andreas</option> <option value="Spider-Man Miles Morales">Spider-Man Miles Morales</option> </select>',
-      focusConfirm: false,
-      customClass: { popup: 'modalAddPost' },
-      preConfirm: () => {
-        return [
-          (<HTMLInputElement>document.getElementById('swal-input1')).value,
-          (<HTMLInputElement>document.getElementById('swal-input2')).value,
-        ];
-      },
-    });
-    if (formValues) {
-      let post: Post = new Post();
-      post.text = formValues[0];
-      post.userId = this.user.id;
-      post.game = formValues[1];
-      this._postService.createPost(post).subscribe((res) => {
-        this._postService.getPostsForUser(this.user.id).subscribe(
-          (result) => {
-            this.posts = result;
-            this.posts.forEach((post) => {
-              post.avatarPath = 'https://localhost:44324/' + post.avatarPath;
-              let contents = [];
-              post.contents.forEach((content) => {
-                content = 'https://localhost:44324/' + content;
-                contents.push(content);
-              });
-              post.contents = contents;
-            });
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
-
-        this.cdr.detectChanges();
-      });
-    }
+    //this._domService.appendComponentToBody(CreatePostComponent);
+    this.chosenModal = this._modalService.show(selectedModal);
   }
 
   //selectedModal: TemplateRef<any>
   public async openPhotoPostModal() {
+    alert('TODO')
     //this.chosenModal = this._modalService.show(selectedModal);
-    const { value: formValues } = await Swal.fire({
-      title: 'Add a photo post',
-      html:
-        '<textarea id="swal-input1" class="swal2-input"></textarea>' +
-        '<select id="swal-input2" class="swal2-input"><option value="No game">No game</option>' +
-        '<option value="Gta V">Gta V</option><option value="Gta San Andreas">Gta San Andreas</option> <option value="Spider-Man Miles Morales">Spider-Man Miles Morales</option> </select>' +
-        '<input type="file" class="swal2-input" id="swal-input3">',
-      focusConfirm: false,
-      customClass: { popup: 'modalAddPost' },
-      preConfirm: () => {
-        return [
-          (<HTMLInputElement>document.getElementById('swal-input1')).value,
-          (<HTMLInputElement>document.getElementById('swal-input2')).value,
-          (<HTMLInputElement>document.getElementById('swal-input3')).value,
-        ];
-      },
-    });
-
-    if (formValues) {
-      let post: Post = new Post();
-      const value = formValues[2].split('\\');
-      post.contents = [value[value.length - 1]];
-      post.text = formValues[0];
-      post.userId = this.user.id;
-      post.game = formValues[1];
-      this._postService.createPost(post).subscribe((res) => {
-        this._postService.getPostsForUser(this.user.id).subscribe(
-          (result) => {
-            this.posts = result;
-            this.posts.forEach((post) => {
-              post.avatarPath = 'https://localhost:44324/' + post.avatarPath;
-              let contents = [];
-              post.contents.forEach((content) => {
-                content = 'https://localhost:44324/' + content;
-                contents.push(content);
-              });
-              post.contents = contents;
-            });
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
-
-        this.cdr.detectChanges();
-      });
-    }
   }
 
   public async openVideoPostModal() {
+    alert('TODO')
     //this.chosenModal = this._modalService.show(selectedModal);
-    const { value: formValues } = await Swal.fire({
-      title: 'Add a Video post',
-      html:
-        '<textarea id="swal-input1" class="swal2-input"></textarea>' +
-        '<select id="swal-input2" class="swal2-input"><option value="No game">No game</option>' +
-        '<option value="Gta V">Gta V</option><option value="Gta San Andreas">Gta San Andreas</option> <option value="Spider-Man Miles Morales">Spider-Man Miles Morales</option><option value="Fifa 21">Fifa 21</option> </select>' +
-        '<input type="file" class="swal2-input" id="swal-input3" multiple>',
-      focusConfirm: false,
-      customClass: { popup: 'modalAddPost' },
-      preConfirm: () => {
-        return [
-          (<HTMLInputElement>document.getElementById('swal-input1')).value,
-          (<HTMLInputElement>document.getElementById('swal-input2')).value,
-          (<HTMLInputElement>document.getElementById('swal-input3')).value,
-        ];
-      },
-    });
-
-    if (formValues) {
-      let post: Post = new Post();
-      const value = formValues[2].split('\\');
-      post.contents = [value[value.length - 1]];
-      post.text = formValues[0];
-      post.userId = this.user.id;
-      post.game = formValues[1];
-      this._postService.createPost(post).subscribe((res) => {
-        this._postService.getPostsForUser(this.user.id).subscribe(
-          (result) => {
-            this.posts = result;
-            this.posts.forEach((post) => {
-              post.avatarPath = 'https://localhost:44324/' + post.avatarPath;
-              let contents = [];
-              post.contents.forEach((content) => {
-                content = 'https://localhost:44324/' + content;
-                contents.push(content);
-              });
-              post.contents = contents;
-            });
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
-
-        this.cdr.detectChanges();
-      });
-    }
   }
 
   public closeModal(data: boolean): void {
