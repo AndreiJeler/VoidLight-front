aa6831d3d76f07ddd19b40124f9b83f858f4be80
Andrei Jeler <andrei.jeler@fabritglobal.com>
2021-01-17 09:24:03 +0200

    const

--- src/app/shared/utils/constants.ts
+++ src/app/shared/utils/constants.ts
@@ -1,9 +1,9 @@
 export class Constants {
-  public static SERVER_URL = 'https://localhost:44324/api';
-  public static SERVER_BASE_URL = 'https://localhost:44324';
+  public static SERVER_URL = 'http://www.voidlight.cyou:5000/api';
+  public static SERVER_BASE_URL = 'https://www.voidlight.cyou:5000';
   public static AUTH_CURRENT_USER_KEY = 'currentUser';
   public static NOT_FOUND_URL = '/notfound';
   public static NOT_AUTHORIZED_URL = '/unauthorized';
   public static DISCORD_OAUTH_URL =
-    'https://discord.com/api/oauth2/authorize?client_id=795466159800844310&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fdiscord-return&response_type=code&scope=identify%20email%20guilds.join%20guilds';
+    'https://discord.com/api/oauth2/authorize?client_id=795466159800844310&redirect_uri=htttp%3A%2F%2Fwww.voidlight.cyou%2Fdiscord-return&response_type=code&scope=email%20identify%20guilds%20guilds.join';
 }
